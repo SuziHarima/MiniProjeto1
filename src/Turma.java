62ffd4755305d4703b277f9e867a377f6cb4ccfb
Matheus <112503237+MatthewsVieira@users.noreply.github.com>
2024-02-14 17:29:49 -0300

    Cria Turma c/ atributos e encapsulamento

---
+++ src/Turma.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class Turma {
+
+    private ArrayList<Aluno> listaAlunos;
+    private int ano;
+    private Curso curso;
+
+    public Turma(Curso curso) {
+        this.listaAlunos = new ArrayList<>();
+        this.curso = curso;
+        Calendar cal = GregorianCalendar.getInstance();
+        this.ano = cal.get(Calendar.YEAR);
+    }
+
+    public ArrayList<Aluno> getListaAlunos() {
+        return listaAlunos;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public Curso getCurso() {
+        return curso;
+    }
+
+    public void setCurso(Curso curso) {
+        this.curso = curso;
+    }
+    
+
+}
