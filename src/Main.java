917a54a39423e27048291ae647061cbd3b853f80
David Dutra <dadutra@hotmail.com>
2024-02-15 16:45:20 -0300

    Criada a classe Professor, presente modificadores de acesso, sobrecarga e toString (override).

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        Professor novoProfessor = new Professor("Fulano de Tal", 40, 2);
+
+        System.out.println(novoProfessor);
+
+        sc.close();
 
     }
 }
