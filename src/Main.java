6d4ecaaea393a7d3f30e84d15bfc4e1a698764cb
David Dutra <dadutra@hotmail.com>
2024-02-17 15:25:38 -0300

    Criadas as classes Funcionario, Diretor e interface IFuncionario

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
 
+        Diretor novoDiretor = new Diretor("Joselito", 2);
+
+        System.out.println(novoDiretor);
+
     }
 }
